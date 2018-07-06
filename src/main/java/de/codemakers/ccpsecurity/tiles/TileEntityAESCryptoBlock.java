72ac4c65d6102c0f9eb29fae75a1a63fc8be2ee8
Paul Hagedorn <panzer1119@gmail.com>
2018-07-06 17:51:09 +0200

    Added AES GCM en/decryption
    Signed-off-by: Paul Hagedorn <panzer1119@gmail.com>

--- src/main/java/de/codemakers/ccpsecurity/tiles/TileEntityAESCryptoBlock.java
+++ src/main/java/de/codemakers/ccpsecurity/tiles/TileEntityAESCryptoBlock.java
@@ -28,6 +28,7 @@ import net.minecraft.tileentity.TileEntity;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
+import javax.crypto.SecretKey;
 
 public class TileEntityAESCryptoBlock extends TileEntity implements ICCPSPeripheral {
     
@@ -60,31 +61,108 @@ public class TileEntityAESCryptoBlock extends TileEntity implements ICCPSPeriphe
     @Nonnull
     @Override
     public String[] getMethodNames() {
-        return new String[] {"generateKey", "test"};
+        return new String[] {"generateAESKey", "randomAESKey", "encryptAESGCM", "decryptAESGCM"};
     }
     
     @Nullable
     @Override
     public Object[] callMethod(@Nonnull IComputerAccess computer, @Nonnull ILuaContext context, int method, @Nonnull Object[] arguments) throws LuaException, InterruptedException {
+        SecretKey key = null;
         switch (method) {
             case 0:
                 if (arguments.length >= 1 && arguments.length <= 2) {
-                    if (!(arguments[0] instanceof String)) {
-                        throw new LuaException("'generateKey' takes a string as the first argument");
+                    if (!(arguments[0] instanceof String) && arguments[0] != null) {
+                        throw new LuaException("'generateAESKey' takes a string as the first argument");
                     } else if (arguments.length >= 2 && !(arguments[1] instanceof Double)) {
-                        throw new LuaException("'generateKey' takes a number as the second argument");
+                        throw new LuaException("'generateAESKey' takes a number as the second argument");
                     } else {
-                        return new Object[] {AES.generateKey("" + arguments[0], arguments.length <= 1 ? -1 : (int) ((double) arguments[1])).getEncoded()};
+                        if (arguments[0] == null) {
+                            if (arguments.length > 1) {
+                                final int size = ((int) ((double) arguments[1]));
+                                key = getSecretKey(size);
+                            } else {
+                                key = AES.generate128Key();
+                            }
+                        } else {
+                            key = AES.generateKey("" + arguments[0], arguments.length <= 1 ? -1 : (int) ((double) arguments[1]));
+                        }
+                        if (key == null) {
+                            return new Object[] {null};
+                        }
+                        return new Object[] {new String(key.getEncoded()), key.getAlgorithm(), key.getFormat()};
                     }
                 } else if (arguments.length == 0) {
-                    throw new LuaException("'generateKey' needs at least 1 argument");
+                    throw new LuaException("'generateAESKey' needs at least 1 argument");
                 } else {
-                    throw new LuaException("'generateKey' only accepts 1 or 2 arguments");
+                    throw new LuaException("'generateAESKey' only accepts 1 or 2 arguments");
                 }
             case 1:
-                return new Object[] {"Test: " + Math.random()};
+                if (arguments.length <= 1) {
+                    if (arguments.length >= 1 && !(arguments[0] instanceof Double)) {
+                        throw new LuaException("'randomAESKey' takes a number as the first argument");
+                    } else {
+                        if (arguments.length > 0) {
+                            final int size = ((int) ((double) arguments[0]));
+                            key = getSecretKey(size);
+                        } else {
+                            key = AES.generate128Key();
+                        }
+                        if (key == null) {
+                            return new Object[] {null};
+                        }
+                        return new Object[] {new String(key.getEncoded()), key.getAlgorithm(), key.getFormat()};
+                    }
+                } else {
+                    throw new LuaException("'randomAESKey' only accepts up to 1 argument");
+                }
+            case 2:
+                if (arguments.length == 2) {
+                    if (!(arguments[0] instanceof String)) {
+                        throw new LuaException("'encryptAESGCM' takes a string as the first argument");
+                    } else if (!(arguments[1] instanceof String)) {
+                        throw new LuaException("'encryptAESGCM' takes a string as the second argument");
+                    } else {
+                        try {
+                            final byte[] encrypted = AES.encrypt(AES.unserialiseKey("" + arguments[0]), ("" + arguments[1]).getBytes());
+                            return new Object[] {new String(encrypted), true};
+                        } catch (Exception ex) {
+                            return new Object[] {null, false, ex.getMessage()};
+                        }
+                    }
+                } else {
+                    throw new LuaException("'encryptAESGCM' needs 2 arguments");
+                }
+            case 3:
+                if (arguments.length == 2) {
+                    if (!(arguments[0] instanceof String)) {
+                        throw new LuaException("'decryptAESGCM' takes a string as the first argument");
+                    } else if (!(arguments[1] instanceof String)) {
+                        throw new LuaException("'decryptAESGCM' takes a string as the second argument");
+                    } else {
+                        try {
+                            final byte[] decrypted = AES.decrypt(AES.unserialiseKey("" + arguments[0]), ("" + arguments[1]).getBytes());
+                            return new Object[] {new String(decrypted), true};
+                        } catch (Exception ex) {
+                            return new Object[] {null, false, ex.getMessage()};
+                        }
+                    }
+                } else {
+                    throw new LuaException("'decryptAESGCM' needs 2 arguments");
+                }
         }
         return new Object[0];
+    }
+    
+    private SecretKey getSecretKey(int size) {
+        SecretKey key;
+        if (size == 256) {
+            key = AES.generate256Key();
+        } else if (size == 192) {
+            key = AES.generate192Key();
+        } else {
+            key = AES.generate128Key();
+        }
+        return key;
     }
     
     @Override
--- src/main/java/de/codemakers/ccpsecurity/utils/AES.java
+++ src/main/java/de/codemakers/ccpsecurity/utils/AES.java
@@ -16,14 +16,24 @@
 
 package de.codemakers.ccpsecurity.utils;
 
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.GCMParameterSpec;
+import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
+import java.nio.ByteBuffer;
 import java.security.MessageDigest;
+import java.security.SecureRandom;
 import java.util.Arrays;
 
 public class AES {
     
     public static MessageDigest SHA_256;
     public static MessageDigest SHA_512;
+    public static KeyGenerator keyGenerator_128;
+    public static KeyGenerator keyGenerator_192;
+    public static KeyGenerator keyGenerator_256;
     
     static {
         try {
@@ -33,6 +43,24 @@ public class AES {
         }
         try {
             SHA_512 = MessageDigest.getInstance("SHA-512");
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        try {
+            keyGenerator_128 = KeyGenerator.getInstance("AES");
+            keyGenerator_128.init(128);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        try {
+            keyGenerator_192 = KeyGenerator.getInstance("AES");
+            keyGenerator_192.init(192);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        try {
+            keyGenerator_256 = KeyGenerator.getInstance("AES");
+            keyGenerator_256.init(256);
         } catch (Exception ex) {
             ex.printStackTrace();
         }
@@ -55,6 +83,89 @@ public class AES {
             size = 128;
         }
         return new SecretKeySpec(Arrays.copyOf(SHA_256.digest(key), size / 8), "AES");
+    }
+    
+    public static SecretKey generate128Key() {
+        return keyGenerator_128.generateKey();
+    }
+    
+    public static SecretKey generate192Key() {
+        return keyGenerator_192.generateKey();
+    }
+    
+    public static SecretKey generate256Key() {
+        return keyGenerator_256.generateKey();
+    }
+    
+    
+    public static SecretKey unserialiseKey(String key) {
+        return unserialiseKey(key, "AES");
+    }
+    
+    public static SecretKey unserialiseKey(String key, String algorithm) {
+        return new SecretKeySpec(key.getBytes(), algorithm);
+    }
+    
+    public static byte[] encrypt(SecretKey key, byte[] data) throws Exception {
+        final int ivSize = 12;
+        final byte[] iv = new byte[ivSize];
+        final SecureRandom random = new SecureRandom();
+        random.nextBytes(iv);
+        final GCMParameterSpec gcmParameterSpec = new GCMParameterSpec(128, iv);
+        final Cipher cipher = Cipher.getInstance("AES/GCM/NoPadding");
+        cipher.init(Cipher.ENCRYPT_MODE, key, gcmParameterSpec);
+        final byte[] encrypted = cipher.doFinal(data);
+        final ByteBuffer byteBuffer = ByteBuffer.allocate(4 + iv.length + encrypted.length);
+        byteBuffer.putInt(iv.length);
+        byteBuffer.put(iv);
+        byteBuffer.put(encrypted);
+        final byte[] encryptedIVAndText = byteBuffer.array();
+        Arrays.fill(encrypted, (byte) 0);
+        byteBuffer.clear();
+        return encryptedIVAndText;
+    }
+    
+    public static byte[] decrypt(SecretKey key, byte[] encryptedIVAndText) throws Exception {
+        final ByteBuffer byteBuffer = ByteBuffer.wrap(encryptedIVAndText);
+        final int ivSize = byteBuffer.getInt();
+        final byte[] iv = new byte[ivSize];
+        byteBuffer.get(iv);
+        final byte[] encrypted = new byte[byteBuffer.remaining()];
+        byteBuffer.get(encrypted);
+        final Cipher cipher = Cipher.getInstance("AES/GCM/NoPadding");
+        cipher.init(Cipher.DECRYPT_MODE, key, new GCMParameterSpec(128, iv));
+        final byte[] data = cipher.doFinal(encrypted);
+        byteBuffer.clear();
+        Arrays.fill(encrypted, (byte) 0);
+        return data;
+    }
+    
+    public static byte[] encryptCBC(SecretKey key, byte[] data) throws Exception {
+        final int ivSize = 16;
+        final byte[] iv = new byte[ivSize];
+        final SecureRandom random = new SecureRandom();
+        random.nextBytes(iv);
+        final IvParameterSpec ivParameterSpec = new IvParameterSpec(iv);
+        final Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+        cipher.init(Cipher.ENCRYPT_MODE, key, ivParameterSpec);
+        byte[] encrypted = cipher.doFinal(data);
+        byte[] encryptedIVAndText = new byte[ivSize + encrypted.length];
+        System.arraycopy(iv, 0, encryptedIVAndText, 0, ivSize);
+        System.arraycopy(encrypted, 0, encryptedIVAndText, ivSize, encrypted.length);
+        return encryptedIVAndText;
+    }
+    
+    public static byte[] decryptCBC(SecretKey key, byte[] encryptedIVAndText) throws Exception {
+        int ivSize = 16;
+        final byte[] iv = new byte[ivSize];
+        System.arraycopy(encryptedIVAndText, 0, iv, 0, iv.length);
+        final IvParameterSpec ivParameterSpec = new IvParameterSpec(iv);
+        int encryptedSize = encryptedIVAndText.length - ivSize;
+        final byte[] encrypted = new byte[encryptedSize];
+        System.arraycopy(encryptedIVAndText, ivSize, encrypted, 0, encryptedSize);
+        final Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+        cipher.init(Cipher.DECRYPT_MODE, key, ivParameterSpec);
+        return cipher.doFinal(encrypted);
     }
     
 }
